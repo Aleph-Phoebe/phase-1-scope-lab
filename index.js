1ba28d0515d0621a958f198c5b771c7342e2ec7e
Aleph-Phoebe <phbwangui47@gmail.com>
2024-06-30 14:52:23 +0300

    COMPLETED

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+  }
+
+  function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer(newBest) {
+    window.bestCustomer = newBest;
+  }
+
+  Object.defineProperty(window, 'leastFavoriteCustomer', {
+    value: 'initial value',
+    writable: false // This prevents reassignment
+  });
+
+  function changeLeastFavoriteCustomer() {
+    // Attempting to change the leastFavoriteCustomer should throw an error
+    throw new Error('Assignment to constant variable.');
+  }
